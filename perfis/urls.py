a0981bc9d6d53aa3677ee40cd3f1643e747ac748
Pauloh <paulork07@gmail.com>
2017-09-19 10:36:16 -0300

    url manipulation

--- perfis/urls.py
+++ perfis/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
     # url(r'^$', 'connectedin.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$','perfis.views.index'),
+    url(r'^perfis/(?P<perfil_id>\d+)$','perfis.views.exibir')
 )
--- perfis/views.py
+++ perfis/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from perfis.models import Perfil
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def exibir(request,perfil_id):
+
+    perfil = Perfil()
+
+    if perfil_id == '1':
+        perfil = Perfil('Paulo Henrique','ph@yopmail.com','1234567','home')
+
+    if perfil_id == '2':
+        perfil = Perfil('Dev','dev@yopmail.com','89123456','work')
+
+
+    return render(request, 'perfil.html',{"perfil" : perfil})
